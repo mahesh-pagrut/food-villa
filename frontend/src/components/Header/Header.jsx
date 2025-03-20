ed4793b40205869fc2d481e7526911ab3a21af4f
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-20 18:06:42 +0530

    components footer, header, navbar, pages = home , cart, place order and css aslo added styling

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+        <h2>Order Now & Satisfy Your Cravings with Every Bite!</h2>
+        <p>Choose from a delicious menu of expertly crafted dishes, made with the freshest ingredients to satisfy your cravings.</p>
+        <button>
+            View Menu
+        </button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets';
+import { FiSearch} from "react-icons/fi";
+import { FaShoppingBasket } from "react-icons/fa";
 
 const Navbar = () => {
+    const [menu, setMenu] = useState('home')
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <img src={assets.logo} alt='logo' className='logo'/>
+        <ul className="navbar-menu">
+            <li onClick={() =>setMenu("home")} className={menu === 'home' ? "active" : ""}>home</li>
+            <li onClick={() =>setMenu("menu")} className={menu === 'menu' ? "active" : ""}>menu</li>
+            <li onClick={() =>setMenu("mobile-app")} className={menu === 'mobile-app' ? "active" : ""}>mobile-app</li>
+            <li onClick={() =>setMenu("contact us")} className={menu === 'contact us' ? "active" : ""}>contact us</li>
+        </ul>
+        <div className="navbar-right">
+            <FiSearch className="icon" />
+            <div className='navbar-search-icon'>
+                <FaShoppingBasket className="icon" />
+                <div className='dot'></div>
+            </div>
+            <button className='sign-in'>Sign In</button>
+        </div>
+    </div>
   )
 }
 
