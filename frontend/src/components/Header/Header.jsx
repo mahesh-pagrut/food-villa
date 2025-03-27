79d8dff7e7e41742a08eebb01d1407c9292dce93
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-27 16:38:35 +0530

    header responsivness

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className='header-contents'>
-        <h2>Order Now & Satisfy Your Cravings with Every Bite!</h2>
+        <h2>Order. Eat. Enjoy. Repeat!</h2>
         <p>Choose from a delicious menu of expertly crafted dishes, made with the freshest ingredients to satisfy your cravings.</p>
         <button>
             View Menu
