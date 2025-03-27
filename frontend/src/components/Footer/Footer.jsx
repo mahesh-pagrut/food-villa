4b90cb8e9ee998486dec7c3355d81311bd82834d
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-27 15:49:27 +0530

    footer html structure jsx component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -19,12 +19,26 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer-content-center">
-
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
             </div>
             <div className="footer-content-right">
-
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+91-0000-000-000</li>
+                    <li>contact@example.com</li>
+                </ul>
             </div>
         </div>
+        <hr/>
+        <p className='footer-copyright'>
+         © 2025  YumStore. All rights reserved. | Your favorite dishes, delivered fresh! 
+        </p>
 
     </div>
   )
