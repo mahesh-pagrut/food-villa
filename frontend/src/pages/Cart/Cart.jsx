ad9a50800e6efff8fea287fffbb4f486f24255df
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-27 23:03:45 +0530

    logic and ui of cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -21,13 +21,17 @@ const Cart = () => {
             if(cartItems[item._id]>0)
             {
               return (
+                <div>
+
                 <div className='cart-items-title cart-items-item'>
                     <img src={item.image} alt="item-image" />
                     <p>{item.name}</p>
-                    <p>{item.price}</p>
-                    <p>{cartItems[item.id]}</p>
-                    <p>{item.price*cartItems[item._id]}</p>
-                    <p>X</p>
+                    <p>${item.price}</p>
+                    <p>{cartItems[item._id]}</p>
+                    <p>${item.price*cartItems[item._id]}</p>
+                    <p onClick={() =>removeFromCart(item._id)} className='cross'>X</p>
+                </div>
+                  <hr/>
                 </div>
               )
             }
