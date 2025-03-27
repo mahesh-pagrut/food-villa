b9919534f0906ccae992e9de6be2f3259093e701
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-27 16:07:56 +0530

    done app download jsx component with html structure

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -2,10 +2,18 @@
 
 import React from 'react'
 import './AppDownload.css'
+import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
   return (
-    <div>AppDownload</div>
+    <div className='app-download' id='app-download'>
+        <p>For Better Experience Download <br/> YumStore </p>
+        <div className="app-download-platforms">
+            <img src={assets.play_store} alt="play_store" />
+            <img src={assets.app_store} alt="app_store" />
+        </div>
+
+    </div>
   )
 }
 
