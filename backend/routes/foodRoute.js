41107b5454d9af1ce4f094ec2a4e392c592380e9
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-31 19:47:45 +0530

    addlist , remove item done

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addFood, listFood } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 import path from "path";
 
@@ -20,5 +20,6 @@ const upload = multer({ storage });
 // Upload route
 foodRouter.post("/add", upload.single("image"), addFood);
 foodRouter.get("/list", listFood)
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
