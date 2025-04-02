c02ede37bcd783f3ed5c5446bde79d1ec2153185
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-02 13:06:48 +0530

    routes for side bar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
+import {Route, Routes} from 'react-router-dom'
+import List from './pages/List/List'
+import Order from './pages/Orders/Order'
+import Add from './pages/Add/Add'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr/>
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Order/>}/>
+        </Routes>
       </div>
     </div>
   )
