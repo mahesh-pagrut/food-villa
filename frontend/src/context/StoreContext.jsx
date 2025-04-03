9d5b60f995fb13dbddc5f518c2e7497812cf2b8d
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-03 21:14:02 +0530

    debug after reloding auto logout

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 import { food_list } from "../assets/assets"; 
 
@@ -34,6 +34,12 @@ const StoreContextProvider = (props) => {
 
         return totalAmount;
     }
+
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"));
+        }
+    },[])
 
     const contextValue = {
         food_list,
