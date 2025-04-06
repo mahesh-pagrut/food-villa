af3dc1e5beef6b51135b17544b68545f90f7b866
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-06 18:22:44 +0530

    personal orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -58,4 +58,16 @@ const placeOrder = async (req, res)=>{
     }
 }
 
-export {placeOrder};
+// user orders fro frontend
+const userOrders = async (req,res) => {
+    try {
+        
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true, data:orders})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:"Error"})
+    }
+}
+
+export {placeOrder,userOrders};
