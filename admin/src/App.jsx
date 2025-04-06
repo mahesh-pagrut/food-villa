86b749f90219715642eb32e3f8b1ee29854f8c04
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-07 01:30:14 +0530

    adimin order structure

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Sidebar from './components/sidebar/Sidebar'
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import List from './pages/List/List'
 import Order from './pages/Orders/Order'
 import Add from './pages/Add/Add'
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
-
   const url = "http://localhost:4000"
-  
+
   return (
     <div>
-      <ToastContainer/>
-      <Navbar/>
-      <hr/>
+      <ToastContainer />
+      <Navbar />
+      <hr />
       <div className="app-content">
-        <Sidebar/>
+        <Sidebar />
         <Routes>
-          <Route path='/add' element={<Add url={url}/>}/>
-          <Route path='/list' element={<List url={url}/>}/>
-          <Route path='/orders' element={<Order url={url}/>}/>
+          {/* 👇 This will redirect / to /add */}
+          <Route path='/' element={<Navigate to="/add" replace />} />
+          <Route path='/add' element={<Add url={url} />} />
+          <Route path='/list' element={<List url={url} />} />
+          <Route path='/orders' element={<Order url={url} />} />
         </Routes>
       </div>
     </div>
--- admin/src/pages/Orders/Order.jsx
+++ admin/src/pages/Orders/Order.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
-import './Order.css'
+import React from "react";
+import "./Order.css";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import axios from "axios";
+import { assets } from "../../assets/assets";
 
-const Order = () => {
+const Order = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
   return (
-    <div>Order</div>
-  )
-}
+    <div>
+      <div className="order add">
+        <h3>Order Page</h3>
+        <div className="order-list">
+          {orders.map((order, index) => {
+            return (
+              <div key={index} className="order-item">
+                <img src={assets.parcel_icon} alt="image" />
+                <div>
+                  <p className="order-item-food">
+                    {order.items.map((item, i) => {
+                      if (i === order.items.length - 1) {
+                        return `${item.name} x ${item.quantity}`;
+                      } else {
+                        return `${item.name} x ${item.quantity}, `;
+                      }
+                    })}
+                  </p>
+                  <p className="order-item-name">
+                    {order.address.firstName + " " + order.address.lastName}
+                  </p>
+                  <div className="order-item-address">
+                    {order.address.street}, {order.address.city},{" "}
+                    {order.address.state} - {order.address.zipcode}
+                  </div>
+                  <p className="order-item-phone">
+                    {order.address.phone}
+                  </p>
+                </div>
+                <p>Items : {order.items.length}</p>
+                <p>${order.amount}</p>
+                <select>
+                  <option value="Food Processing">Food Processing</option>
+                  <option value="Out for delivery">Out for delivery</option>
+                  <option value="Delivered">Delivered</option>
+                </select>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Order
+export default Order;
