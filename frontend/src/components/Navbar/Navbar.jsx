a0208a45567152a09151511d40e845b3d41e20a2
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-04-08 02:18:10 +0530

    admin panel issue resolve

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,57 +1,97 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets';
-import { FiSearch} from "react-icons/fi";
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { FiSearch } from "react-icons/fi";
 import { IoMdCart } from "react-icons/io";
-import { Link, useNavigate } from 'react-router-dom';
-import { StoreContext } from '../../context/StoreContext';
+import { Link, useNavigate } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
-const Navbar = ({setShowLogin}) => {
-    const [menu, setMenu] = useState('home')
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
 
-    const {getTotalCartAmount, token, setToken}= useContext(StoreContext);
-    const navigate = useNavigate();
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+  const navigate = useNavigate();
 
-    const logout = () => {
-        localStorage.removeItem("token")
-        setToken("")
-        navigate("/")
-
-    }
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  };
 
   return (
-    <div className='navbar'>
-        <Link to='/'>
-        <img src={assets.logo} alt='logo' className='logo'/>
+    <div className="navbar">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
         </Link>
-        <ul className="navbar-menu">
-            <Link to='/' onClick={() =>setMenu("home")} className={menu === 'home' ? "active" : ""}>home</Link>
-            <a href='#explore-menu' onClick={() =>setMenu("menu")} className={menu === 'menu' ? "active" : ""}>menu</a>
-            <a href='#app-download' onClick={() =>setMenu("mobile-app")} className={menu === 'mobile-app' ? "active" : ""}>mobile-app</a>
-            <a href='#footer' onClick={() =>setMenu("contact us")} className={menu === 'contact us' ? "active" : ""}>contact us</a>
-        </ul>
-        <div className="navbar-right">
-            <FiSearch className="icon" />
-            <div className='navbar-search-icon'>
-                <Link to="/cart">
-                <IoMdCart className="icon" />
-                </Link>
-                <div className={getTotalCartAmount()===0?"":"dot"}></div>
-            </div>
-            {!token?<button className='sign-in' onClick={() => setShowLogin(true)}>Sign In</button>
-            
-        :<div className='navbar-profile'>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact us")}
+          className={menu === "contact us" ? "active" : ""}
+        >
+          contact us
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <FiSearch className="icon" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <IoMdCart className="icon" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+        </div>
+        {!token ? (
+          <button className="sign-in" onClick={() => setShowLogin(true)}>
+            Sign In
+          </button>
+        ) : (
+          <div className="navbar-profile">
             <img src={assets.profile_icon} alt="profile" />
             <ul className="navbar-profile-dropdown">
-                <li onClick={()=> navigate('/myorders')}><img src={assets.bag_icon} alt="logout_icon" />Orders</li>
-                <hr />
-                <li onClick={logout}><img src={assets.logout_icon} alt="logout_icon" />Logout</li>
+              <li onClick={() => navigate("/myorders")}>
+                <img src={assets.bag_icon} alt="orders_icon" /> Orders
+              </li>
+              <hr />
+              <li>
+                <a
+                  href="https://food-del-admin-orpin.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={assets.profile_icon} alt="admin_icon" /> Admin Panel
+                </a>
+              </li>
+              <hr />
+              <li onClick={logout}>
+                <img src={assets.logout_icon} alt="logout_icon" /> Logout
+              </li>
             </ul>
-            </div>}
-            
-        </div>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
