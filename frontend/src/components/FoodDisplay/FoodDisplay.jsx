ec81591348e19944123a72767975bc61ea97d00f
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-05-16 18:57:42 +0530

    fixed bug

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
@@ -6,36 +6,42 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 10000); // 5 seconds delay
+
+    return () => clearTimeout(timer); // cleanup
+  }, []);
 
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list && food_list.length > 0 ? (
-          food_list.map((item) => {
-            if (category === "All" || category === item.category) {
-              return (
-                <FoodItem
-                  key={item._id}
-                  id={item._id}
-                  name={item.name}
-                  description={item.description}
-                  price={item.price}
-                  image={item.image}
-                />
-              );
-            } else {
-              return null;
-            }
-          })
-        ) : (
+        {loading || !food_list || food_list.length === 0 ? (
           <div className="loading-lottie">
             <DotLottieReact
               src="https://lottie.host/a9cfd994-3472-45fc-a1d5-e4e304bec762/6OfLqdXF9q.lottie"
               loop
               autoplay
             />
+            <p className="loading-text">Simmering something delicious for you...</p>
           </div>
+        ) : (
+          food_list.map((item) =>
+            category === "All" || category === item.category ? (
+              <FoodItem
+                key={item._id}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            ) : null
+          )
         )}
       </div>
     </div>
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
 const Header = () => {
-  return (
-    <div className='header'>
-        <div className='header-contents'>
-        <h2>Order. Eat. Enjoy. Repeat!</h2>
-        <p>Choose from a delicious menu of expertly crafted dishes, made with the freshest ingredients to satisfy your cravings.</p>
-        <button>
-            View Menu
-        </button>
-        </div>
-    </div>
-  )
-}
+  const scrollToMenu = () => {
+    const element = document.getElementById('explore-menu');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-export default Header
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order. Eat. Enjoy. Repeat!</h2>
+        <p>
+          Choose from a delicious menu of expertly crafted dishes, made with the freshest ingredients to satisfy your cravings.
+        </p>
+        <button onClick={scrollToMenu}>
+          View Menu
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
